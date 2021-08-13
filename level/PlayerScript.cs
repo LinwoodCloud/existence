a6d4ec24c23be6b02cfafbb405da620c58985d13
CodeDoctor <wusel905@mail.com>
2021-08-13 21:01:27 +0200

    add player and placeholder textures

--- level/PlayerScript.cs
+++ level/PlayerScript.cs
@@ -5,27 +5,75 @@ namespace ExistenceDot.Level
     public class PlayerScript : KinematicBody
     {
         [Export()]
-        private int speed;
-        private AnimationTree animTree;
-        private readonly NodePath animTreePath = new NodePath("AnimationTree");
+        private float _gravity = -9.8f;
+        [Export()]
+        private int _speed = 6;
+        [Export()]
+        private int _acceleration = 3;
+        [Export()]
+        private int _deAcceleration = 5;
+        private Vector3 _velocity;
+        private AnimationTree _animTree;
+        private Camera _camera;
 
         public override void _Ready()
         {
-            animTree = GetNode<AnimationTree>(animTreePath);
+            _animTree = GetNode<AnimationTree>("AnimationTree");
+            _camera = GetNode<Camera>("Target/Camera");
+            SetPhysicsProcess(true);
         }
 
         public override void _PhysicsProcess(float delta)
         {
-            var stateMachine = (AnimationNodeStateMachinePlayback)animTree.Get("parameters/playback");
-            var rootMotion = animTree.GetRootMotionTransform();
-            var v = rootMotion.origin / delta;
-            GD.Print(stateMachine.GetCurrentNode());
+            var dir = new Vector3();
+            var gt = _camera.GlobalTransform;
+            var isMoving = false;
+            if (Input.IsActionPressed("move_forward"))
+            {
+                dir += -gt.basis[2];
+                isMoving = true;
+            }
+            if (Input.IsActionPressed("move_backward"))
+            {
+                dir += gt.basis[2];
+                isMoving = true;
+            }
+            if (Input.IsActionPressed("move_left"))
+            {
+                dir += -gt.basis[0];
+                isMoving = true;
+            }
+            if (Input.IsActionPressed("move_right"))
+            {
+                dir += gt.basis[0];
+                isMoving = true;
+            }
+            dir.y = 0;
+            dir = dir.Normalized();
+            _velocity.y += delta * _gravity;
+            var hv = _velocity;
+            hv.y = 0;
+            var newPos = dir * _speed;
+            var accel = _deAcceleration;
+            if (dir.Dot(hv) > 0)
+                accel = _acceleration;
+            hv = hv.LinearInterpolate(newPos, accel * delta);
+            _velocity.x = hv.x;
+            _velocity.z = hv.z;
+            _velocity = MoveAndSlide(_velocity, new Vector3(0, 1, 0));
+            if (isMoving)
+            {
+                var angle = Mathf.Atan2(hv.x, hv.z);
+                var charRot = Rotation;
+                charRot.y = angle;
+                Rotation = charRot;
+            }
+            var stateMachine = (AnimationNodeStateMachinePlayback)_animTree.Get("parameters/playback");
             var playingAnim = "Idle";
-            if (Input.IsActionPressed("ui_select"))
+            if (isMoving)
                 playingAnim = "Walking";
-            if(playingAnim != stateMachine.GetCurrentNode())
+            if (playingAnim != stateMachine.GetCurrentNode())
                 stateMachine.Travel(playingAnim);
-            MoveAndSlide(v, Vector3.Up);
         }
     }
 }
--- main/MainMenuScript.cs
+++ main/MainMenuScript.cs
@@ -11,6 +11,12 @@ namespace ExistenceDot
 		// Called when the node enters the scene tree for the first time.
 		public override void _Ready()
 		{
+			GetNode<Button>("VBoxController/StartButton").Connect("pressed", this, nameof(StartGame));
+			GetNode<Button>("VBoxController/OptionsButton").Connect("pressed", this, nameof(OpenOptions));
+			GetNode<Button>("VBoxController/WebsiteButton").Connect("pressed", this, nameof(OpenWebsite));
+			GetNode<Button>("VBoxController/CommunityButton").Connect("pressed", this, nameof(OpenCommunity));
+			GetNode<Button>("VBoxController/CreditsButton").Connect("pressed", this, nameof(OpenCredits));
+			GetNode<Button>("VBoxController/QuitButton").Connect("pressed", this, nameof(QuitGame));
 		}
 
 		public void ConfirmQuit()
@@ -30,7 +36,22 @@ namespace ExistenceDot
 
 		public void OpenWebsite()
 		{
-			OS.ShellOpen("https://cosmocity.linwood.dev");
+			OS.ShellOpen("https://existence.linwood.dev");
+		}
+
+		public void StartGame()
+		{
+			GetTree().ChangeScene("res://level/LevelScene.tscn");
+		}
+
+		public void OpenOptions()
+		{
+		}
+		public void OpenCommunity()
+		{
+		}
+		public void OpenCredits()
+		{
 		}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
